88b89e07289ca866f2e12ae19e8240043d147160
zhuangyan <zy_php@163.com>
2018-01-30 16:53:01 +0800

    init

---
+++ back/djangos/djangos/urls.py
@@ -0,0 +1,47 @@
+"""djangos URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from rest_framework_jwt.views import obtain_jwt_token
+
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^admin/', admin.site.urls),
+    # url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^api-token-auth/', obtain_jwt_token),
+
+]
